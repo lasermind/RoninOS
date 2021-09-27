d4d6430e6dbd3646908377dc7bae7dbe6af3c5c5
likewhoa <likewhoa@weboperative.com>
2021-09-26 22:25:04 -0400

    Code clean up and fixes

--- overlays/RoninOS/opt/ronin-oem-fast.sh
+++ overlays/RoninOS/opt/ronin-oem-fast.sh
@@ -1,112 +1,91 @@
 #!/bin/bash
 
 TMPDIR=/var/tmp
-SYSTEM=`inxi -M | awk '{print $6}'`
-SYSTEMPRO=`inxi -M | awk '{print $7,$8}'`
-USERGROUPS=""
 USER="admin"
 PASSWORD="admin"
-CONFIRMPASSWORD="admin"
 ROOTPASSWORD="ronindojoroot"
-CONFIRMROOTPASSWORD="ronindojoroot"
 FULLNAME="RoninDojo"
 TIMEZONE="UTF-8"
 LOCALE="en_US.UTF-8"
 HOSTNAME="RoninDojo"
 KEYMAP="us"
-#IP_ADDRESS=$(ip route get 1.1.1.1 | awk '{print$7}')
-#NETWORK_INTERFACE=$(ip route get 1.1.1.1 | awk '{print$5}')
-#GATEWAY=$(ip route get 1.1.1.1 | awk '{print$3}')
 
 create_oem_install() {
     echo "$USER" > $TMPDIR/user
     echo "$PASSWORD" >> $TMPDIR/password
-    echo "$PASSWORD" >> $TMPDIR/password
     echo "$ROOTPASSWORD" >> $TMPDIR/rootpassword
-    echo "$ROOTPASSWORD" >> $TMPDIR/rootpassword
-#    tr -dc 'a-zA-Z0-9' </dev/urandom | fold -w 8 | head -n 1 >> /tmp/adminpass
-#    ADMINPASS=$(cat /tmp/adminpass)
-#    echo $ADMINPASS >> /tmp/adminpass
+
     echo "Setting root password..."
-    passwd root < $TMPDIR/rootpassword 1> /dev/null 2>&1
+    passwd root < $TMPDIR/rootpassword &>/dev/null
+
     echo "Adding user $USER..."
-    useradd -m -G wheel,sys,audio,input,video,storage,lp,network,users,power -s /bin/bash $(cat $TMPDIR/user) 1> /dev/null 2>&1
-    if [ -d /usr/share/sddm ]; then
-    cp /usr/share/sddm/faces/.face.icon /usr/share/sddm/faces/$USER.face.icon
-    fi
-    usermod -aG $USERGROUPS $(cat $TMPDIR/user) 1> /dev/null 2>&1
+    useradd -m -G wheel,sys,audio,input,video,storage,lp,network,users,power -s /bin/bash "$(cat $TMPDIR/user)" &>/dev/null
+
+    usermod -aG "$USERGROUPS" "$(cat $TMPDIR/user)" &>/dev/null
+
     echo "Setting full name to $FULLNAME..."
-    chfn -f "$FULLNAME" $(cat $TMPDIR/user) 1> /dev/null 2>&1
+    chfn -f "$FULLNAME" "$(cat $TMPDIR/user)" &>/dev/null
+
     echo "Setting password for $USER..."
-    passwd $(cat $TMPDIR/user) < $TMPDIR/password 1> /dev/null 2>&1
+    passwd "$(cat $TMPDIR/user)" < $TMPDIR/password &>/dev/null
+
     echo "Setting timezone to $TIMEZONE..."
-    timedatectl set-timezone $TIMEZONE 1> /dev/null 2>&1
-    timedatectl set-ntp true 1> /dev/null 2>&1
+    timedatectl set-timezone $TIMEZONE &>/dev/null
+    timedatectl set-ntp true &>/dev/null
+
     echo "Generating $LOCALE locale..."
-    sed -i s/"#$LOCALE"/"$LOCALE"/g /etc/locale.gen 1> /dev/null 2>&1
-    locale-gen 1> /dev/null 2>&1
-    localectl set-locale $LOCALE 1> /dev/null 2>&1
-    if [[ "$SYSTEM" != "Pinebook" ]]; then
-        echo "Setting keymap to $KEYMAP..."
-    localectl set-keymap $KEYMAP 1> /dev/null 2>&1
-    fi
+    sed -i "s/#$LOCALE/$LOCALE/" /etc/locale.gen &>/dev/null
+    locale-gen &>/dev/null
+    localectl set-locale $LOCALE &>/dev/null
+
     if [ -f /etc/sway/inputs/default-keyboard ]; then
-    sed -i s/"us"/"$KEYMAP"/ /etc/sway/inputs/default-keyboard
-    if [[ "$KEYMAP" = "uk" ]]; then
-    sed -i s/"uk"/"gb"/ /etc/sway/inputs/default-keyboard
+        sed -i "s/us/$KEYMAP/" /etc/sway/inputs/default-keyboard
+
+        if [[ "$KEYMAP" = "uk" ]]; then
+            sed -i "s/uk/gb/" /etc/sway/inputs/default-keyboard
+        fi
     fi
-    fi
+
     echo "Setting hostname to $HOSTNAME..."
-    hostnamectl set-hostname $HOSTNAME 1> /dev/null 2>&1
+    hostnamectl set-hostname $HOSTNAME &>/dev/null
+
     echo "Resizing partition..."
-    resize-fs 1> /dev/null 2>&1
-    echo "Applying system settings..."
-    #systemctl disable systemd-resolved.service 1> /dev/null 2>&1
-    systemctl enable ronin-setup.service
-    #systemctl start avahi.service
+    resize-fs &>/dev/null
+
     echo "Cleaning install for unwanted files..."
     sudo rm -rf /var/log/*
-    
+
     # Remove temp files on host
-    sudo rm -rf $TMPDIR/user $TMPDIR/password $TMPDIR/rootpassword
+    sudo rm -f $TMPDIR/user $TMPDIR/password $TMPDIR/rootpassword
 
     loadkeys "$KEYMAP"
 
     echo "Configuration complete. Cleaning up..."
-    #mv /usr/lib/systemd/system/getty@.service.bak /usr/lib/systemd/system/getty@.service
     rm /root/.bash_profile
-    sed -i s/"PermitRootLogin yes"/"#PermitRootLogin prohibit-password"/g /etc/ssh/sshd_config
-    sed -i s/"PermitEmptyPasswords yes"/"#PermitEmptyPasswords no"/g /etc/ssh/sshd_config
+
+    sed -i -e "s/PermitRootLogin yes/#PermitRootLogin prohibit-password/" \
+        -e "s/PermitEmptyPasswords yes/#PermitEmptyPasswords no/" /etc/ssh/sshd_config
 }
 
-if [ "$(systemctl is-active dhcpcd.service 2>/dev/null)" != "active" ]; then 
-   systemctl enable --now dhcpcd.service 
+if ! systemctl is-active --quiet dhcpcd.service; then
+   systemctl enable --quiet --now dhcpcd.service
 fi
 
-if [ "$(systemctl is-active avahi-daemon.service 2>/dev/null)" != "active" ]; then
-   systemctl disable systemd-resolved.service 1> /dev/null 2>&1
-   systemctl enable --now avahi-daemon.service
+if ! systemctl is-active --quiet avahi-daemon.service; then
+   systemctl disable systemd-resolved.service &>/dev/null
+   systemctl enable --quiet --now avahi-daemon.service
 fi
 
 create_oem_install
 
-#chmod 0640 /etc/shadow
-#passwd -e admin
-
-if [ "$(systemctl is-enabled motd.service 2>/dev/null)" != "enabled" ]; then
-   systemctl enabled --now motd.service
+if ! systemctl is-enabled motd.service; then
+   systemctl enabled --quiet --now motd.service
 fi
 
-echo '' >> /etc/resolv.conf
-#echo '# Added at Ronin startup' >> /etc/resolv.conf
-echo 'domain .local' >> /etc/resolv.conf
-echo 'nameserver 8.8.8.8' >> /etc/resolv.conf
-echo 'nameserver 8.8.4.4' >> /etc/resolv.conf
-echo 'nameserver 1.1.1.1' >> /etc/resolv.conf
+echo -e "domain .local\nnameserver 8.8.8.8\nnameserver 8.8.4.4\nnameserver 1.1.1.1" >> /etc/resolv.conf
 
 echo "OEM complete"
 cat /etc/motd
 
-systemctl enable ronin-setup.service
-systemctl disable oem-boot.service
-systemctl reboot
+systemctl enable --quiet ronin-setup.service
+systemctl disable --quiet oem-boot.service
