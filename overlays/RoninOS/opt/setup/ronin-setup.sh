15b2116a4af3f469c196c1e2d483beab3972ab2c
likewhoa <likewhoa@weboperative.com>
2021-10-06 13:32:31 -0400

    Clean up

--- overlays/RoninOS/opt/setup/ronin-setup.sh
+++ overlays/RoninOS/opt/setup/ronin-setup.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-systemctl disable oem-boot.service
-
-mv /opt/RoninDojo /home/admin/RoninDojo
-chown -R admin:admin /home/admin/RoninDojo
+# Enable password less sudo
 echo "admin ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/99-nopasswd
-
-# move repo
 
 su admin -c /home/admin/RoninDojo/ronin _main
 source /home/admin/RoninDojo/Scripts/defaults.sh /home/admin/RoninDojo/Scripts/functions.sh && su admin -c /home/admin/RoninDojo/Scripts/functions.sh _main
 su admin -c /home/admin/RoninDojo/Scripts/Install/install-system-setup.sh
 su admin -l /home/admin/RoninDojo/Scripts/Install/install-dojo.sh
 
-rm  /etc/sudoers.d/99-nopasswd
+rm /etc/sudoers.d/99-nopasswd
 systemctl disable ronin-setup.service
-done
