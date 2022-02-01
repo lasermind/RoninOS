4d6b46be3a4128148e264c7d043f6b7f7765f3f9
BTCxZelko <btcxzelko@protonmail.com>
2022-02-01 07:17:11 -0800

    enabled post-ronin service

--- overlays/RoninOS/usr/local/sbin/ronin-setup.sh
+++ overlays/RoninOS/usr/local/sbin/ronin-setup.sh
@@ -23,5 +23,6 @@ if _main; then
     sudo systemctl daemon-reload
 
     sudo systemctl disable ronin-setup.service
+    sudo systemctl enable ronin-post.service
     sudo rm /etc/sudoers.d/99-nopasswd
 fi
