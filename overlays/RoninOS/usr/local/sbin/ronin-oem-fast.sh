0308adab2cfcd8b01f494462b8cb19bfc0020761
BTCxZelko <BTCxZelko@protonmail.com>
2023-02-28 18:24:32 -0800

    add a check to make sure logs dir and files are created before running ronin-setup.service

--- overlays/RoninOS/usr/local/sbin/ronin-oem-fast.sh
+++ overlays/RoninOS/usr/local/sbin/ronin-oem-fast.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+if [ ! -d /home/ronindojo/.logs ]; then
+    mkdir -p /home/ronindojo/.logs
+    touch /home/ronindojo/.logs/setup.logs
+    touch /home/ronindojo/.logs/post.logs
+    chown -R ronindojo:ronindojo /home/ronindojo/.logs
+fi
+
 systemctl enable ronin-setup.service
 systemctl disable oem-boot.service
