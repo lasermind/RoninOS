fa656b3e41e11c15911ee7c81d9688cd78f82913
BTCxZelko <BTCxZelko@protonmail.com>
2023-03-01 21:37:29 -0800

    reworked to include Ronin-UI installed during build

--- overlays/customize-image.sh
+++ overlays/customize-image.sh
@@ -108,12 +108,8 @@ EOF
     chown -R ronindojo:ronindojo /home/ronindojo/.logs
 }
 
-_service_checks(){
-    if ! systemctl is-enabled --quiet dhcpcd.service; then
-        systemctl enable --quiet dhcpcd.service
-    fi
-    
-    systemctl disable tor
+_service_checks(){  
+    systemctl enable tor
 
     if ! systemctl is-enabled --quiet avahi-daemon.service; then
         systemctl disable systemd-resolved.service &>/dev/null
@@ -150,13 +146,82 @@ _prep_install(){
 
     # Clone Repo
     git clone -b feature/debian_mode https://code.samourai.io/ronindojo/RoninDojo /home/ronindojo/RoninDojo
+}
 
+_install_ronin_ui(){
+    . /home/ronindojo/RoninDojo/Scripts/generated-credentials.sh
+    . /home/ronindojo/RoninDojo/Scripts/defaults.sh
+    . /home/ronindojo/RoninDojo/Scripts/
+
+    cd /home/ronindojo || exit
+
+    _print_message "Checking package dependencies for Ronin UI..."
+    _sleep
+
+    _install_pkg_if_missing "nginx"
+    _install_pkg_if_missing "avahi-daemon"
+
+    sudo npm i -g pnpm@7 &>/dev/null
+
+    #sudo npm install pm2 -g
+
+    test -d "${ronin_ui_path}" || mkdir "${ronin_ui_path}"
+    cd "${ronin_ui_path}" || exit
+
+    wget -q "${roninui_version_file}" -O /tmp/version.json 2>/dev/null
+
+    _file=$(jq -r .file /tmp/version.json)
+    _shasum=$(jq -r .sha256 /tmp/version.json)
+
+    wget -q https://ronindojo.io/downloads/RoninUI/"$_file" 2>/dev/null
+
+    if ! echo "${_shasum} ${_file}" | sha256sum --check --status; then
+        _bad_shasum=$(sha256sum ${_file})
+        _print_error_message "Ronin UI archive verification failed! Valid sum is ${_shasum}, got ${_bad_shasum} instead..."
+    fi
+      
+    tar xzf "$_file"
+
+    rm "$_file" /tmp/version.json
+
+        # Mark Ronin UI initialized if necessary
+        if [ -e "${ronin_ui_init_file}" ]; then
+          echo -e "{\"initialized\": true}\n" > ronin-ui.dat
+        fi
+
+        # Generate .env file
+        echo "JWT_SECRET=$gui_jwt" > .env
+        echo "NEXT_TELEMETRY_DISABLED=1" >> .env
+
+    if [ "${roninui_version_staging}" = true ] ; then
+        echo -e "VERSION_CHECK=staging\n" >> .env
+    fi
+
+    _print_message "Performing pnpm install, please wait..."
+
+    sudo -u ronindojo pnpm install --prod &>/dev/null || { printf "\n%s***\nRonin UI pnpm install failed...\n***%s\n" "${red}" "${nc}";exit; }
+
+    _print_message "Performing Next start, please wait..."
+
+    sudo -u ronindojo pm2 start pm2.config.js
+    sudo -u ronindojo pm2 save
+    sudo -u ronindojo pm2 startup && env PATH="$PATH:/usr/bin" /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u "ronindojo" --hp "/home/ronindojo" &>/dev/null
+
+    _ronin_ui_setup_tor
+
+    _ronin_ui_vhost
+
+    _ronin_ui_avahi_service
+
+    chown -R ronindojo:ronindojo /home/ronindojo/Ronin-UI
 }
 
 _prep_tor(){
 	mkdir -p /mnt/usb/tor
 	chown -R tor:tor /mnt/usb/tor
 	sed -i '$a\User tor\nDataDirectory /mnt/usb/tor' /etc/tor/torrc
+    cp /home/ronindojo/RoninDojo/example.tor.service /usr/lib/systemd/system/tor.service
+    rm -rf /usr/lib/systemd/system/tor@* #remove unnecessary debian installed services
 }
 
 main(){
@@ -176,13 +241,15 @@ main(){
         exit 1;
     else 
         echo "Setup service is PRESENT! Keep going!"
-        _prep_install
         _create_oem_install
+        _prep_install
         _service_checks
+        _prep_tor
         usermod -aG pm2 ronindojo
-        usermod -aG docker ronindojo
         mkdir -p /usr/share/nginx/logs
         rm -rf /etc/nginx/sites-enabled/default
+        _install_ronin_ui
+        usermod -aG docker ronindojo
         systemctl enable oem-boot.service
         systemctl enable ronin-setup.service
         systemctl enable ronin-post.service
