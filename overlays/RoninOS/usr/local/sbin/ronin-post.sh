43c763dd77e5c9e916b27d2af28d0d3a9f69121e
BTCxZelko <BTCxZelko@protonmail.com>
2023-03-07 23:12:58 -0800

    fix pm2 and getty calls that broke the script.

--- overlays/RoninOS/usr/local/sbin/ronin-post.sh
+++ overlays/RoninOS/usr/local/sbin/ronin-post.sh
@@ -6,8 +6,10 @@ do
     sleep 5s
     if ! systemctl is-active --quiet ronin-setup.service
     then
-        echo "restarting pm2..."
-        pm2 resurrect
+        sudo systemctl disable ronin-setup.service
+        sudo systemctl disable ronin-post.service
+        touch /home/ronindojo/.logs/post-complete
+        echo "Install complete"
         break
     fi
 done
