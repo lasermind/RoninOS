0434e05206ee3245cc36bbf895202b98102badd7
BTCxZelko <BTCxZelko@protonmail.com>
2023-03-22 15:28:12 -0700

    Add sudo to chpasswd command. turn into a function and call inside if statement.

--- overlays/RoninOS/usr/local/sbin/ronin-setup.sh
+++ overlays/RoninOS/usr/local/sbin/ronin-setup.sh
@@ -1,37 +1,43 @@
 #!/bin/bash
-# Generate a random password consisting of 16 characters using only numbers and letters.
-ROOTPASSWORD="$(tr -dc 'a-zA-Z0-9' </dev/urandom | head -c'21')"
-PASSWORD="$(tr -dc 'a-zA-Z0-9' </dev/urandom | head -c'21')"
-USER="ronindojo"
+
+regenerate_passwords_and_update_info_file(){
+    # Generate a random password consisting of 16 characters using only numbers and letters.
+    ROOTPASSWORD="$(tr -dc 'a-zA-Z0-9' </dev/urandom | head -c'21')"
+    PASSWORD="$(tr -dc 'a-zA-Z0-9' </dev/urandom | head -c'21')"
+    USER="ronindojo"
 
 
-# Change the root user's password.
-echo "Changing the root user's password..."
-chpasswd <<<"root:$ROOTPASSWORD"
+    # Change the root user's password.
+    echo "Changing the root user's password..."
+    sudo chpasswd <<<"root:$ROOTPASSWORD"
 
-# Change the ronindojo user's password.
-echo "Changing the ronindojo user's password..."
-chpasswd <<<"$USER:$PASSWORD"
+    # Change the ronindojo user's password.
+    echo "Changing the ronindojo user's password..."
+    sudo chpasswd <<<"$USER:$PASSWORD"
 
-# remove old info.json then create new with new passwords.
-rm -rf /home/ronindojo/.config/RoninDojo/info.json
-cat <<EOF >/home/"${USER}"/.config/RoninDojo/info.json
+    # remove old info.json then create new with new passwords.
+    rm -rf /home/ronindojo/.config/RoninDojo/info.json
+    cat <<EOF >/home/"${USER}"/.config/RoninDojo/info.json
 {"user":[{"name":"${USER}","password":"${PASSWORD}"},{"name":"root","password":"${ROOTPASSWORD}"}]}
 EOF
 
-# add validation for that the setup was done.
-GENERATE_MESSAGE="Generated during system Setup."
-TIMESTAMP=$(date)
-echo "${GENERATE_MESSAGE}$'\n'${TIMESTAMP}">> /home/"${USER}"/.config/RoninDojo/info.json
+    # add validation for that the setup was done.
+    GENERATE_MESSAGE="Generated during system Setup."
+    TIMESTAMP=$(date)
+    echo "${GENERATE_MESSAGE}" >> /home/"${USER}"/.config/RoninDojo/info.json
+    echo "${TIMESTAMP}" >> /home/"${USER}"/.config/RoninDojo/info.json
+}
 
-# Source files
 cd "$HOME"/RoninDojo || exit
-
+# Source files
 . Scripts/defaults.sh
 . Scripts/functions.sh
 
 # Run main
 if _main; then
+    # regenerate the passwords to be random for each user.
+    regenerate_passwords_and_update_info_file
+
     # Run system setup
     Scripts/Install/install-system-setup.sh system
 
