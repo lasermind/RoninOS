01aa4ca4b2a4b3e1112e0ecf04070446037c4843
BTCxZelko <BTCxZelko@protonmail.com>
2023-03-21 18:22:07 -0700

    Fix issue #23 from Ronin-Test. generate new passwords during setup to ensure that passwords are random for every user

--- overlays/RoninOS/usr/local/sbin/ronin-setup.sh
+++ overlays/RoninOS/usr/local/sbin/ronin-setup.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
+# Generate a random password consisting of 16 characters using only numbers and letters.
+ROOTPASSWORD="$(tr -dc 'a-zA-Z0-9' </dev/urandom | head -c'21')"
+PASSWORD="$(tr -dc 'a-zA-Z0-9' </dev/urandom | head -c'21')"
+USER="ronindojo"
+
+
+# Change the root user's password.
+echo "Changing the root user's password..."
+chpasswd <<<"root:$ROOTPASSWORD"
+
+# Change the ronindojo user's password.
+echo "Changing the ronindojo user's password..."
+chpasswd <<<"$USER:$PASSWORD"
+
+# remove old info.json then create new with new passwords.
+rm -rf /home/ronindojo/.config/RoninDojo/info.json
+cat <<EOF >/home/"${USER}"/.config/RoninDojo/info.json
+{"user":[{"name":"${USER}","password":"${PASSWORD}"},{"name":"root","password":"${ROOTPASSWORD}"}]}
+EOF
+
+# add validation for that the setup was done.
+GENERATE_MESSAGE="Generated during system Setup."
+TIMESTAMP=$(date)
+echo "${GENERATE_MESSAGE}$'\n'${TIMESTAMP}">> /home/"${USER}"/.config/RoninDojo/info.json
 
 # Source files
 cd "$HOME"/RoninDojo || exit
